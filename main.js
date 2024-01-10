096eda07e07df2f89d1092fa1af534bec091a68a
Vu Dat <vudat42@gmail.com>
2024-01-10 14:06:53 +0700

    render todo status

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 
+function getElement(selector) {
+  if (!selector) return;
 
-console.log(add(1, 2));
+  return document.querySelector(selector);
+}
 
-function renderTodoStatus(todo, alertElement, btnMarkAsDone) {
-  if (!todo || !alertElement || !btnMarkAsDone) return null;
+function renderTodoStatus(todo) {
+  // const { alertElement, btnMarkAsDone } = getElements();
+
+  if (!todo) return;
+
+  const alertElement = getElement('.alert');
+  const btnMarkAsDone = getElement('#todoList button.mark-as-done');
+  console.log(alertElement, btnMarkAsDone);
 
   // change alert color
   const alertClass =
@@ -20,85 +29,78 @@ function renderTodoStatus(todo, alertElement, btnMarkAsDone) {
   btnMarkAsDone.textContent = btnMarkAsDoneContent;
 }
 
-function handleButtonRemove(btnRemove, todoElement, todo) {
-  if (!btnRemove || !todoElement) return null;
+// function handleButtonRemove(todo) {
+//   const btnRemove = getElement('#todoList button.remove');
+//   console.log(btnRemove);
 
-  btnRemove.addEventListener("click", () => {
-    // save to local storage
-    const todoList = getTodoList();
-    const newTodoList = todoList.filter((x) => x.id !== todo.id);
-    localStorage.setItem("todo_list", JSON.stringify(newTodoList));
+//   if (!btnRemove) return;
 
-    // apply to DOM
-    todoElement.remove();
-  });
-}
+//   btnRemove.addEventListener("click", () => {
+//     console.log('btnRemove');
+//     // save to local storage
+//     const todoList = getTodoList();
+//     const newTodoList = todoList.filter((x) => x.id !== todo.id);
+//     localStorage.setItem("todo_list", JSON.stringify(newTodoList));
 
-function handleButtonMarkAsDone(
-  todo,
-  btnMarkAsDone,
-  alertElement,
-  todoElement
-) {
-  if (!todo || !btnMarkAsDone || !alertElement || !todoElement) return null;
+//     // apply to DOM
+//     todoElement.remove();
+//   });
+// }
 
-  btnMarkAsDone.addEventListener("click", () => {
-    const currentStatus = todoElement.dataset.status;
-    const newStatus = currentStatus === "pending" ? "completed" : "pending";
+// function handleButtonMarkAsDone(todo) {
+//   const { btnMarkAsDone, alertElement, todoElement } = getElements();
 
-    // save to local storage
-    const todoList = getTodoList();
-    const index = todoList.findIndex((x) => x.id === todo.id);
-    todoList[index].status = newStatus;
-    localStorage.setItem("todo_list", JSON.stringify(todoList));
+//   if (!todo || !btnMarkAsDone || !alertElement || !todoElement) return null;
 
-    // change alert
-    const newAlertClass =
-      currentStatus === "pending" ? "alert-success" : "alert-secondary";
-    alertElement.classList.remove("alert-success", "alert-secondary");
-    alertElement.classList.add(newAlertClass);
-    todoElement.dataset.status = newStatus;
+//   btnMarkAsDone.addEventListener("click", () => {
+//     const currentStatus = todoElement.dataset.status;
+//     const newStatus = currentStatus === "pending" ? "completed" : "pending";
 
-    // change button color and content
-    const newBtnMarkAsDoneStyle =
-      currentStatus === "pending" ? "btn-success" : "btn-dark";
-    btnMarkAsDone.classList.remove("btn-success", "btn-dark");
-    btnMarkAsDone.classList.add(newBtnMarkAsDoneStyle);
+//     // save to local storage
+//     const todoList = getTodoList();
+//     const index = todoList.findIndex((x) => x.id === todo.id);
+//     todoList[index].status = newStatus;
+//     localStorage.setItem("todo_list", JSON.stringify(todoList));
 
-    const newBtnMarkAsDoneContent =
-      currentStatus === "pending" ? "Reset" : "Finish";
-    btnMarkAsDone.textContent = newBtnMarkAsDoneContent;
-  });
-}
+//     // change alert
+//     const newAlertClass =
+//       currentStatus === "pending" ? "alert-success" : "alert-secondary";
+//     alertElement.classList.remove("alert-success", "alert-secondary");
+//     alertElement.classList.add(newAlertClass);
+//     todoElement.dataset.status = newStatus;
+
+//     // change button color and content
+//     const newBtnMarkAsDoneStyle =
+//       currentStatus === "pending" ? "btn-success" : "btn-dark";
+//     btnMarkAsDone.classList.remove("btn-success", "btn-dark");
+//     btnMarkAsDone.classList.add(newBtnMarkAsDoneStyle);
+
+//     const newBtnMarkAsDoneContent =
+//       currentStatus === "pending" ? "Reset" : "Finish";
+//     btnMarkAsDone.textContent = newBtnMarkAsDoneContent;
+//   });
+// }
 
 function createTodoElement(todo) {
-  if (!todo) return null;
+  if (!todo) return;
 
-  const todoTemplate = document.getElementById("todoTemplate");
-  if (!todoTemplate) return null;
-
-  // get elements
+  const todoTemplate = getElement('#todoTemplate');
+  if (!todoTemplate) return;
   const todoElement = todoTemplate.content.firstElementChild.cloneNode(true);
-  const alertElement = todoElement.querySelector("div.alert");
-  const btnMarkAsDone = todoElement.querySelector("button.mark-as-done");
-  const btnRemove = todoElement.querySelector("button.remove");
-  const btnEdit = todoElement.querySelector('button.edit');
+  if (!todoElement) return;
 
   todoElement.querySelector("p.todo__title").textContent = todo.title;
   todoElement.dataset.status = todo.status;
   todoElement.dataset.id = todo.id;
 
   // render todo status
-  renderTodoStatus(todo, alertElement, btnMarkAsDone);
+  renderTodoStatus(todo);
 
   // handle button remove
-  handleButtonRemove(btnRemove, todoElement, todo);
+  // handleButtonRemove(todo);
 
   // handle button mark as done
-  handleButtonMarkAsDone(todo, btnMarkAsDone, alertElement, todoElement);
-
-  // handle button Edit
-  hanleButtonEdit(todo, btnEdit, todoElement);
+  // handleButtonMarkAsDone(todo);
 
   return todoElement;
 }
@@ -106,8 +108,8 @@ function createTodoElement(todo) {
 function renderTodoList(todoList, ulElementId) {
   if (!Array.isArray(todoList) || todoList.length === 0) return null;
 
-  const ulElement = document.getElementById(ulElementId);
-  if (!ulElement) return null;
+  const ulElement = getElement(`#${ulElementId}`);
+  if (!ulElement) return;
 
   for (const todo of todoList) {
     const liElement = createTodoElement(todo);
@@ -124,48 +126,32 @@ function getTodoList() {
   }
 }
 
-function hanleTodoFormSubmit(event) {
-  event.preventDefault();
-  
-  const todoInput = document.getElementById('todoText');
-  if (!todoInput) return;
+// function handleTodoFormSubmit(event) {
+//   event.preventDefault();
 
-  const newTodo = {
-    id: Date.now(),
-    title: todoInput.value,
-    status: 'pending'
-  }
+//   const todoInput = document.getElementById('todoText');
+//   if (!todoInput) return;
 
-  
+//   const newTodo = {
+//     id: Date.now(),
+//     title: todoInput.value,
+//     status: 'pending'
+//   }
 
-  const todoList = getTodoList();
-  todoList.push(newTodo);
-  localStorage.setItem('todo_list', JSON.stringify(todoList));
+//   const todoList = getTodoList();
+//   todoList.push(newTodo);
+//   localStorage.setItem('todo_list', JSON.stringify(todoList));
 
-  const newLiElement = createTodoElement(newTodo);
-  const ulElement = document.getElementById('todoList');
-  if (!ulElement) return;
-  ulElement.appendChild(newLiElement);
+//   const newLiElement = createTodoElement(newTodo);
+//   const ulElement = document.getElementById('todoList');
+//   if (!ulElement) return;
+//   ulElement.appendChild(newLiElement);
 
-  const todoForm = document.getElementById("todoFormId");
-  if (todoForm) todoForm.reset();
-  todoInput.focus();
-}
+//   const todoForm = document.getElementById('todoFormId');
+//   todoForm.reset();
+//   todoInput.focus();
+// }
 
-function hanleButtonEdit(todo, btnEdit, todoElement) {
-  btnEdit.addEventListener('click', () => {
-      // const todoText = todoElement.querySelector('p.todo__title').textContent;
-      const todoText = todo.title;
-      if (!todoText) return;
-      const todoForm = document.getElementById("todoFormId");
-      if (!todoForm) return;
-      const todoInput = todoForm.querySelector('#todoText');
-      if (!todoInput) return;
-      todoInput.value = todoText;
-      todoForm.dataset.id = todo.id;
-  })
-}
-  
 (() => {
   // const todoList = [
   //   { id: 1, title: "HTML & CSS", status: "pending" },
@@ -176,8 +162,9 @@ function hanleButtonEdit(todo, btnEdit, todoElement) {
 
   renderTodoList(todoList, "todoList");
 
-  const todoForm = document.getElementById("todoFormId");
-  if (todoForm) {
-    todoForm.addEventListener("submit", hanleTodoFormSubmit);
-  }
+  // const { todoForm } = getElements();
+
+  // if (todoForm) {
+  //   todoForm.addEventListener("submit", handleTodoFormSubmit);
+  // }
 })();
