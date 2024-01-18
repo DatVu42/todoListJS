4477028891750672f31d6040eca9734b76ab7443
unknown <vudat42@gmail.com>
2024-01-18 19:53:25 +0700

    search feature completed

--- search-todo.js
+++ search-todo.js
@@ -1,13 +1,32 @@
-
-function initSearchInput() {
-    const searchInput = document.getElementById('searchTerm');
-    if (!searchInput) return;
-
-    searchInput.addEventListener('input', () => {
-        console.log(searchInput.value);
-    })
+function getElement(selector) {
+  return document.querySelector(selector);
 }
 
+function isMatch(todoElement, searchTerm) {
+  const titleElement = todoElement.querySelector('p.todo__title');
+    
+  return titleElement.textContent.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase());
+}
+
+function searchTodo(searchTerm) {
+  const todoElementList = document.querySelectorAll('#todoList > li');
+  
+  for (const todoElement of todoElementList) {
+    const isShow = isMatch(todoElement, searchTerm);
+
+    todoElement.hidden = !isShow;
+  }
+}
+
+function initSearchInput() {
+  const searchInput = getElement('#searchTerm');
+
+  searchInput.addEventListener("input", () => {
+    searchTodo(searchInput.value);
+  });
+}
+
+// MAIN
 (() => {
-    initSearchInput();
-})()
+  initSearchInput();
+})();
